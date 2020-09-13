f13eb1857fcbe0de75e4f72a01307dd808bdbfff
desmoteuss <desmoteuss@gmail.com>
2020-09-13 21:16:31 +0200

    singup initialize

--- functions/index.js
+++ functions/index.js
@@ -1,70 +1,66 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin'); 
-const app = require('express') ();
+const functions = require('firebase-functions');
+const admin = require('firebase-admin'); 
+const app = require('express') ();
 admin.initializeApp();
-// const { initializeApp } = require('firebase-admin');
+// const { initializeApp } = require('firebase-admin');
 
 
 
 
 
-
-const config = {
-  apiKey: "AIzaSyAbD24uyELRcQSk4GNrmXjzgZAOCmDri5U",
-  authDomain: "portal-74634.firebaseapp.com",
-  databaseURL: "https://portal-74634.firebaseio.com",
-  projectId: "portal-74634",
-  storageBucket: "portal-74634.appspot.com",
-  messagingSenderId: "653730382236",
-  appId: "1:653730382236:web:4a5faa12de91690caa4859",
-  measurementId: "G-2DPS61WTEF"
+const config = {
+  apiKey: "AIzaSyAbD24uyELRcQSk4GNrmXjzgZAOCmDri5U",
+  authDomain: "portal-74634.firebaseapp.com",
+  databaseURL: "https://portal-74634.firebaseio.com",
+  projectId: "portal-74634",
+  storageBucket: "portal-74634.appspot.com",
+  messagingSenderId: "653730382236",
+  appId: "1:653730382236:web:4a5faa12de91690caa4859",
+  measurementId: "G-2DPS61WTEF"
 };
 
-
-// const app = express();
-
-const firebase = require('firebase');
+// const app = express();
+const firebase = require('firebase');
 firebase.initializeApp(config)
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
-app.get('/posts',(req,res)=> {
-  admin.firestore().collection('posts').orderBy('createdAt','desc').get().then(data => {
-    let posts = [];
-    data.for.Each(doc => {
-        posts.push({
-          postId:doc.id,
-          body: doc.data().body,
-          userHandle: doc.data().userHandle,
-          cretedAt: doc.data().createdAt
-        });
-    });
-    return res.json(posts);
- })
- .catch((err)=> console.error(err));
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+app.get('/posts',(req,res)=> {
+  admin.firestore().collection('posts').orderBy('createdAt','desc').get().then(data => {
+    let posts = [];
+    data.for.Each(doc => {
+        posts.push({
+          postId:doc.id,
+          body: doc.data().body,
+          userHandle: doc.data().userHandle,
+          cretedAt: doc.data().createdAt
+        });
+    });
+    return res.json(posts);
+ })
+ .catch((err)=> console.error(err));
 });
-app.post(`/post`,(req,res) => {
-const newPost = { 
-  body: req.body.body,
-  userHandle: req.body.userHandle,
-  cretedAt : new Date().toISOString()
+app.post(`/post`,(req,res) => {
+const newPost = { 
+  body: req.body.body,
+  userHandle: req.body.userHandle,
+  cretedAt : new Date().toISOString()
 };
 admin.firestore().collection('posts')
 .add(newPost)
-.then((doc) => {
-  res.json ({
-    message:`document ${doc.id } created successfully`
-  });
+.then((doc) => {
+  res.json ({
+    message:`document ${doc.id } created successfully`
+  });
 })
-.catch((err)=> 
-  {
-    res.status(500).json({error:`something went wrong`}) 
-  });
+.catch((err)=> 
+  {
+    res.status(500).json({error:`something went wrong`}) 
+  });
 console.error(err);
 });
 
@@ -72,37 +68,31 @@ console.error(err);
 
 
 
-
 //signup
-
-app.post('signup',(req,res) =>{
-  const newUser = {
-    email: req.body.email,
-    password: req.body.password,
-    confirmPassword: req.body.confirmPassword,
-    handle: req.body.handle,
-  };
-
+app.post('signup',(req,res) =>{
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    handle: req.body.handle,
+  };
 
 
 
 
-//validate date
-
+//validate date
 firebase.auth().createUserWithEmailAndPassword(newUser.email,newUser.password)
-.then((data) =>{
-  return res.status(201).json({message: `user${data.user.uid} signed up successfully`});
-  })
+.then((data) =>{
+  return res.status(201).json({message: `user${data.user.uid} signed up successfully`});
+  })
 .catch((err)=>{
-  console.error(err);
-  return res.status(500).json({error: err.code});
-  });
-
+  console.error(err);
+  return res.status(500).json({error: err.code});
+  });
 });
 
 
 
 
 
-
-exports.api = functions.region('europe-west1').https.onRequest(app);
+exports.api = functions.region('europe-west1').https.onRequest(app);
