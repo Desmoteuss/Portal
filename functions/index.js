42a72fe824edc6f2936bbe358a7511035ed2b83b
desmoteuss <desmoteuss@gmail.com>
2020-09-30 18:45:09 +0200

    checking authentication before adding a post

--- functions/index.js
+++ functions/index.js
@@ -4,9 +4,6 @@ const app = require('express') ();
 admin.initializeApp();
 const { initializeApp } = require('firebase-admin');
 
-
-
-
 const config = {
   apiKey: "AIzaSyAbD24uyELRcQSk4GNrmXjzgZAOCmDri5U",
   authDomain: "portal-74634.firebaseapp.com",
@@ -17,17 +14,12 @@ const config = {
   appId: "1:653730382236:web:4a5faa12de91690caa4859",
   measurementId: "G-2DPS61WTEF"
 };
-// const app = express();
+
+const app = express();
 const firebase = require('firebase');
 firebase.initializeApp(config)
 const db =admin.firestore();
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+
 app.get('/posts',(req,res)=> {
   db.collection('posts').orderBy('createdAt','desc').get().then(data => {
     let posts = [];
@@ -36,17 +28,55 @@ app.get('/posts',(req,res)=> {
           postId:doc.id,
           body: doc.data().body,
           userHandle: doc.data().userHandle,
-          cretedAt: doc.data().createdAt
+          cretedAt: doc.data().createdAt,
+        commentCount: doc.data().commentCount,
+        likeCount: doc.data().likeCount
         });
     });
     return res.json(posts);
- })
- .catch((err)=> console.error(err));
+  })
+ .catch((err)=> {
+console.error(err);
+res.status(500).json({ error: err.code });
+  });
 });
-app.post(`/post`,(req,res) => {
+
+
+const FBAuth=( req,res,next) =>{
+  let idToken;
+  if(req.headers.autorization && req.headers.autorization.startsWith('Bearer ')){
+idToken = req.headers.autorization.split('Bearer ')[1];
+
+  } else{
+    console.error('no token foun')
+    return res.status(403.json({error : ' unauthorized'}));
+  }
+  admin.auth().verifyIdToken(idToken)
+  .then(decodedToken => {
+    req.user = decodedToken;
+    return db.collection('users')
+    .where('userId', '==',req.user.uid)
+    .limit(1)
+    .get();
+  })
+  .then(data = >{
+    req.user.handle = data.docs[0].ata().handle;
+    return next();
+  })
+  .catch(err => {
+    console.error('error while verifing token',err);
+    return res.status(403).json(err);
+  })
+}
+//Post
+
+app.post(`/post`,FBAuth,(req,res) => {
+  if ( req.bidy.body.trim() === '') {
+    return res.status(400).json ( {body: 'body must not be empoty'});
+  }
 const newPost = { 
   body: req.body.body,
-  userHandle: req.body.userHandle,
+  userHandle: req.body.handle,
   cretedAt : new Date().toISOString()
 };
 db.collection('posts')
@@ -62,15 +92,17 @@ db.collection('posts')
   });
 console.error(err);
 });
-const isEmpty = (string) => { 
-if (string.trim() === '' ) return true;
-else return false;
+
+const isEmpty = (string) => { 
+if (string.trim() === '' ) return true;
+else return false;
 }
-const isEmail = (email) => { 
-  const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
- if (email.match(regEx)) return true;
- else return false;
+const isEmail = (email) => { 
+  const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+ if (email.match(regEx)) return true;
+ else return false;
 }
+
 
 
 //signup
@@ -82,24 +114,30 @@ app.post('signup',(req,res) =>{
     handle: req.body.handle,
   };
 
-let errors = {};
-if(isEmpty(newUser.email)) {
-  errors.email = ' cannot be empty'
+
+let errors = {};
+
+if(isEmpty(newUser.email)) {
+  errors.email = ' cannot be empty'
 }
-else if (!isEmail(newUser.email)) { 
-  error.email = " Provide valid email address "
-}
-if(isEmpty(newUser.password)) {
-  errors.password = ' cannot be empty'
-}
-if(newUser.password ~== newUser.confirmPassword) errors.confirmPassword = ' Password must match'
-if(isEmpty(newUser.handle)) {
-  errors.handle = ' cannot be empty'
+else if (!isEmail(newUser.email)) { 
+  error.email = " Provide valid email address "
 }
 
-if(Object.keys(errors).lenght > 0) return res.status(400).json(errors);
+if(isEmpty(newUser.password)) {
+  errors.password = ' cannot be empty'
+}
+if(newUser.password ~== newUser.confirmPassword) errors.confirmPassword = ' Password must match'
+if(isEmpty(newUser.handle)) {
+  errors.handle = ' cannot be empty'
+}
+
+
+if(Object.keys(errors).lenght > 0) return res.status(400).json(errors);
 //validate date
-let token,userId;
+
+let token,userId;
+
 db.doc(`/users/${newUser.handle}`).get()
 .then(doc => {
 if(doc.exists) {
@@ -110,22 +148,24 @@ else{
 }
 })
 .then(data =>{
-  userId = data.user.uid;
+  userId = data.user.uid;
   return data.user.getIDToken()
 })
 .then(idToken =>{
-  token = idToken;
-  const userCredentials = {
-    handle: newUser.handle,
-    email: newUser.email,
-    createdAt: new Date().toISOString(),
-    userId
-  
-  };
-return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+  token = idToken;
+  const userCredentials = {
+    handle: newUser.handle,
+    email: newUser.email,
+    createdAt: new Date().toISOString(),
+    userId
+  
+
+  };
+
+return db.doc(`/users/${newUser.handle}`).set(userCredentials);
 })
-.then(()=> {
-  return res.status(201).json({token});
+.then(()=> {
+  return res.status(201).json({token});
 })
 .catch (err => {
   console.error(err);
@@ -135,35 +175,44 @@ return db.doc(`/users/${newUser.handle}`).set(userCredentials);
   else {
     return res.status(500).json({error: err.code});
   }
-}); 
+}); 
+
 });
-app.post ('./login',(req,res ) =>{
-const user = {
-  email : req.body.email,
-  password: req.body.password 
+
+app.post ('./login',(req,res) =>{
+const user = {
+  email : req.body.email,
+  password: req.body.password 
+
 };
 
-let errors = {};
 
-if ( isEmpty(user.email)) errors.email = ' cannot be empty';
-if ( isEmpty(user.password)) errors.password = ' cannot be empty';
-if (Object.keys(error).lenght > 0) return res.status(400).json(errors);
+let errors = {};
+
+
+if ( isEmpty(user.email)) errors.email = ' cannot be empty';
+if ( isEmpty(user.password)) errors.password = ' cannot be empty';
+if (Object.keys(error).lenght > 0) return res.status(400).json(errors);
+
 firebase.auth()signInWithEmailandPassword(user.email,user.password)
-.then ( data =>{
-  return data.user.getIDToken(); 
+.then ( data =>{
+  return data.user.getIDToken(); 
 })
-.then(token => {
-return res.json({token});
+.then(token => {
+return res.json({token});
+
 }
-.catch ((err) => {
-  console.error(err);
-  if(err.code === 'auth/wrong-password') {
-    return res.status(403).json({general : ' Wrong credentials. Please try again'});
-  }
-  else return res.status(500).json({error: err.code});
+.catch ((err) => {
+  console.error(err);
+  if(err.code === 'auth/wrong-password') {
+    return res.status(403).json({general : ' Wrong credentials. Please try again'});
+  }
+  else return res.status(500).json({error: err.code});
+
 
 });
 })
+
 
 
 
