3070749d5eb80798a2a9d8000ede9348f817ba90
desmoteuss <desmoteuss@gmail.com>
2020-10-21 22:49:19 +0200

    test

--- functions/handlers/posts.js
+++ functions/handlers/posts.js
@@ -1,5 +1,6 @@
 const { db } = require('../util.admin');
 
+
 exports.getAllPosts = (req,res)=> {
       db.collection('posts').orderBy('createdAt','desc').get().then(data => {
         let posts = [];
